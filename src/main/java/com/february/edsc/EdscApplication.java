71e6ed383aba0f47b723c9382ca90f9017a1863d
hoit1302 <jueun000101@gmail.com>
2021-01-31 13:11:53 +0900

    feat(edsc): create project

---
+++ src/main/java/com/february/edsc/EdscApplication.java
@@ -0,0 +1,13 @@
+package com.february.edsc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdscApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EdscApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/february/edsc/EdscApplicationTests.java
@@ -0,0 +1,13 @@
+package com.february.edsc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdscApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
