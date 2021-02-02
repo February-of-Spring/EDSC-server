9bed7d46b6eb4c23dc9079d3bfd214aaa0abf517
jeongwon-iee <garden1117244@gmail.com>
2021-02-02 09:57:15 +0900

    feat(like): add like entity

---
+++ src/main/java/com/february/edsc/domain/Like.java
@@ -0,0 +1,22 @@
+package com.february.edsc.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+public class Like {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "like_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+}
