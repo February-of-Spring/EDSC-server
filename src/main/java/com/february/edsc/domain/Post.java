02edd5fbafaea3a9b1e82a7765b86d0712b6b46c
jeongwon-iee <garden1117244@gmail.com>
2021-02-02 10:08:29 +0900

    feat(post): add post entity

---
+++ src/main/java/com/february/edsc/domain/Post.java
@@ -0,0 +1,41 @@
+package com.february.edsc.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter
+public class Post {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "post_id")
+    private Long id;
+
+    private String title;
+    private String content;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @Column(name = "modified_at")
+    private LocalDateTime modified_At;
+
+    @Column(name = "like_count")
+    private int likeCount;
+
+    @Column(name = "view_count")
+    private int viewCount;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id")
+    private Category category;
+
+}
