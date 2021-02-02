6dc11fc6c32eb156787f15021351d5cca42c1ed4
jeongwon-iee <garden1117244@gmail.com>
2021-02-02 10:22:37 +0900

    feat(image): add image entity

--- src/main/java/com/february/edsc/domain/Image.java
+++ src/main/java/com/february/edsc/domain/Image.java
@@ -1,2 +1,23 @@
-package com.february.edsc.domain;public class Image {
+package com.february.edsc.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter @Setter
+public class Image {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "image_id")
+    private Long id;
+
+    private String path;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "post_id")
+    private Post post;
+
 }
