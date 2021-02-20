85c49ea8ac543e3154b02d70189e1136ba1ec430
hoit1302 <jueun000101@gmail.com>
2021-02-20 12:58:07 +0900

    feat(PostController): add empty user error logic

--- src/main/java/com/february/edsc/controller/PostController.java
+++ src/main/java/com/february/edsc/controller/PostController.java
@@ -1,23 +1,32 @@
 package com.february.edsc.controller;
 
 import com.february.edsc.domain.post.PostRequestDto;
+import com.february.edsc.domain.user.User;
 import com.february.edsc.service.PostService;
+import com.february.edsc.service.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.net.URI;
+import java.util.Optional;
 
 @Controller
 @AllArgsConstructor
 public class PostController {
+	private final UserService userService;
 	private final PostService postService;
 
 	@PostMapping("/posts")
-	public ResponseEntity createPost(@RequestBody PostRequestDto postRequestDto) {
-		String postId = postService.createPost(postRequestDto);
+	public ResponseEntity<Object> createPost(@RequestBody PostRequestDto postRequestDto) {
+		Optional<User> user = userService.findByEmail(postRequestDto.getEmail());
+		if (user.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("존재하지 않는 유저입니다.");
+		}
+		String postId = postService.createPost(postRequestDto, user.get());
 		URI location = URI.create("/posts/" + postId);
 		return ResponseEntity.created(location).build();
 	}
