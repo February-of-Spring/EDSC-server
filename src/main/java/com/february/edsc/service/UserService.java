57b6d47f49c8c825b1f56c22c75765bae2c35e87
hoit1302 <jueun000101@gmail.com>
2021-02-20 23:48:57 +0900

    feat(UserService): add getUserPosts logic

--- src/main/java/com/february/edsc/service/UserService.java
+++ src/main/java/com/february/edsc/service/UserService.java
@@ -1,6 +1,10 @@
 package com.february.edsc.service;
 
+import com.february.edsc.domain.post.Post;
+import com.february.edsc.domain.post.PostListResponseDto;
+import com.february.edsc.domain.post.PostResponseDto;
 import com.february.edsc.domain.user.User;
+import com.february.edsc.repository.PostJpaRepository;
 import com.february.edsc.repository.UserJpaRepository;
 import com.february.edsc.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional(readOnly = true)
@@ -17,6 +22,7 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final UserJpaRepository userJpaRepository;
+    private final PostJpaRepository postJpaRepository;
 
     @Transactional
     public Long join(User user) {
@@ -33,17 +39,22 @@ public class UserService {
         }
     }
 
-    // 회원 전체 조회
-    public List<User> findMembers() {
-        return userRepository.findAll();
-    }
-
-    // 단건 조회
-    public User findOne(Long memberId) {
-        return userRepository.findOne(memberId);
-    }
-
     public Optional<User> findByEmail(String email) {
         return userJpaRepository.findByEmail(email);
+    }
+    
+    public Optional<User> findById(Long memberId) {
+        return userJpaRepository.findById(memberId);
+    }
+
+    @Transactional
+    public PostListResponseDto getUserPosts(Long memberId) {
+        List<PostResponseDto> posts =
+            postJpaRepository.findAllByUserId(memberId).stream()
+                .map(Post::toPostResponseDto).collect(Collectors.toList());
+        return PostListResponseDto.builder()
+            .totalNum(posts.size())
+            .postList(posts)
+            .build();
     }
 }
