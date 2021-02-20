891edf0e54345575eac853ccd71732f0e76ad840
hoit1302 <jueun000101@gmail.com>
2021-02-20 18:30:29 +0900

    feat(ErrorMessage): add error case

--- src/main/java/com/february/edsc/common/ErrorMessage.java
+++ src/main/java/com/february/edsc/common/ErrorMessage.java
@@ -2,4 +2,5 @@ package com.february.edsc.common;
 
 public class ErrorMessage {
 	public static final String NO_SUCH_USER = "존재하지 않는 유저입니다.";
+	public static final String NO_SUCH_POST = "존재하지 않는 게시물입니다.";
 }
