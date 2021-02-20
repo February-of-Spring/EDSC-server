37c29fe0024e30ee63a9d9f773c236b815c40273
hoit1302 <jueun000101@gmail.com>
2021-02-20 12:59:24 +0900

    feat(PostService): add category and user when creating post

--- src/main/java/com/february/edsc/service/PostService.java
+++ src/main/java/com/february/edsc/service/PostService.java
@@ -1,29 +1,43 @@
 package com.february.edsc.service;
 
+import com.february.edsc.domain.category.Category;
 import com.february.edsc.domain.post.Post;
 import com.february.edsc.domain.post.PostRequestDto;
+import com.february.edsc.domain.user.User;
+import com.february.edsc.repository.CategoryJpaRepository;
 import com.february.edsc.repository.PostRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Service
 @AllArgsConstructor
 public class PostService {
+	private final CategoryJpaRepository categoryJpaRepository;
 	private final PostRepository postRepository;
 
 	@Transactional
-	public String createPost(PostRequestDto postRequestDto) {
+	public String createPost(PostRequestDto postRequestDto, User user) {
+		Category category = getCategory(postRequestDto.getCategoryName());
 		Long postId = postRepository.save(
 			Post.builder()
-				.user(postRequestDto.getUser())
+				.user(user)
 				.title(postRequestDto.getTitle())
 				.content(postRequestDto.getContent())
-				.category(postRequestDto.getCategory())
-				.images(postRequestDto.getImages())
-				.files(postRequestDto.getFiles())
+				.category(category)
+//				.images(postRequestDto.getImages())
+//				.files(postRequestDto.getFiles())
 				.build()
 		);
 		return postId.toString();
+	}
+
+	private Category getCategory(String categoryName) {
+		Optional<Category> category = categoryJpaRepository.findByName(categoryName);
+		if (category.isEmpty())
+			return categoryJpaRepository.save(Category.builder().name(categoryName).build());
+		return category.get();
 	}
 }
