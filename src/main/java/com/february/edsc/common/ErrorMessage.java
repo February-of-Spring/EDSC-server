acda7aa9c4a41b34dcd725e45640b06da12aa171
hoit1302 <jueun000101@gmail.com>
2021-02-22 00:37:39 +0900

    feat(ErrorMessage): add error case

--- src/main/java/com/february/edsc/common/ErrorMessage.java
+++ src/main/java/com/february/edsc/common/ErrorMessage.java
@@ -4,4 +4,5 @@ public class ErrorMessage {
 	public static final String NO_SUCH_USER = "존재하지 않는 유저입니다.";
 	public static final String NO_SUCH_POST = "존재하지 않는 게시물입니다.";
 	public static final String REQUIRED_FIELD_NULL = "필수 항목을 입력해주세요.";
+	public static final String NO_SUCH_CATEGORY = "존재하지 않는 카테고리입니다.";
 }
