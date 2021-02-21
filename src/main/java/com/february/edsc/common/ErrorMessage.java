e86e28090a4888350dbf193e745c51f0cf2d431a
hoit1302 <jueun000101@gmail.com>
2021-02-21 12:18:58 +0900

    feat(ErrorMessage): add error message

--- src/main/java/com/february/edsc/common/ErrorMessage.java
+++ src/main/java/com/february/edsc/common/ErrorMessage.java
@@ -3,4 +3,5 @@ package com.february.edsc.common;
 public class ErrorMessage {
 	public static final String NO_SUCH_USER = "존재하지 않는 유저입니다.";
 	public static final String NO_SUCH_POST = "존재하지 않는 게시물입니다.";
+	public static final String REQUIRED_FIELD_NULL = "필수 항목을 입력해주세요.";
 }
