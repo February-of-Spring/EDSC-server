df3d5b88e06cf37ba853fcaa6ffb6d1a9b28f286
hoit1302 <jueun000101@gmail.com>
2021-02-21 23:51:26 +0900

    feat(CategoryResponseDto): add dto

---
+++ src/main/java/com/february/edsc/domain/category/CategoryResponseDto.java
@@ -0,0 +1,15 @@
+package com.february.edsc.domain.category;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class CategoryResponseDto {
+	private final Long id;
+	private final String name;
+	private final int level;
+	private final Long parentCategoryId;
+}
