73e3977d5090f4fee27341552e983b4727c7555c
hoit1302 <jueun000101@gmail.com>
2021-02-22 13:36:20 +0900

    feat(ErrorMessage): add error case

--- src/main/java/com/february/edsc/common/ErrorMessage.java
+++ src/main/java/com/february/edsc/common/ErrorMessage.java
@@ -5,4 +5,6 @@ public class ErrorMessage {
 	public static final String NO_SUCH_POST = "존재하지 않는 게시물입니다.";
 	public static final String REQUIRED_FIELD_NULL = "필수 항목을 입력해주세요.";
 	public static final String NO_SUCH_CATEGORY = "존재하지 않는 카테고리입니다.";
+	public static final String DUPLICATE_CATEGORY = "이미 존재하는 카테고리입니다.";
+	public static final String NO_SUCH_PARENT_CATEGORY = "카테고리의 상위 항목이 존재하지 않습니다.";
 }
