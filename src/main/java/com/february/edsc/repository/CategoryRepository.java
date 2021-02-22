906d4e85138fdb3126380c72eb698c1ba9ccd033
hoit1302 <jueun000101@gmail.com>
2021-02-22 10:02:54 +0900

    feat(CategoryRepository): add findByLevel method

--- src/main/java/com/february/edsc/repository/CategoryRepository.java
+++ src/main/java/com/february/edsc/repository/CategoryRepository.java
@@ -3,8 +3,11 @@ package com.february.edsc.repository;
 import com.february.edsc.domain.category.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface CategoryRepository extends CrudRepository<Category, Long> {
 	Optional<Category> findByName(String category);
+	List<Category> findAllByLevel(int level);
+	List<Category> findAllByParentIdAndLevel(Long parentId, int level);
 }
