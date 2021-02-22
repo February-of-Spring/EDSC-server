fe2862b65510566272c691005541bebcf1914c7c
hoit1302 <jueun000101@gmail.com>
2021-02-22 19:33:33 +0900

    feat(CommetController): add POST /posts/:post_id/comments

---
+++ src/main/java/com/february/edsc/controller/CommentController.java
@@ -0,0 +1,63 @@
+package com.february.edsc.controller;
+
+import com.february.edsc.common.Error;
+import com.february.edsc.common.ErrorMessage;
+import com.february.edsc.domain.post.Post;
+import com.february.edsc.domain.post.comment.Comment;
+import com.february.edsc.domain.post.comment.CommentRequestDto;
+import com.february.edsc.domain.user.User;
+import com.february.edsc.service.CommentService;
+import com.february.edsc.service.PostService;
+import com.february.edsc.service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+import java.net.URI;
+import java.util.Optional;
+
+@Controller
+@AllArgsConstructor
+public class CommentController {
+
+	private final UserService userService;
+	private final PostService postService;
+	private final CommentService commentService;
+
+	@PostMapping("/posts/{postId}/comments")
+	public ResponseEntity<Object> createCategory(
+		@PathVariable Long postId,
+		@RequestBody CommentRequestDto commentRequestDto) {
+		if (commentRequestDto.isRequiredFieldNull())
+			return ResponseEntity.badRequest()
+			.body(new Error(HttpStatus.BAD_REQUEST, ErrorMessage.REQUIRED_FIELD_NULL));
+		Optional<User> user = userService.findByEmail(commentRequestDto.getEmail());
+		if (user.isEmpty()) {
+			return ResponseEntity.badRequest()
+				.body(new Error(HttpStatus.BAD_REQUEST, ErrorMessage.NO_SUCH_USER));
+		}
+		Optional<Post> post = postService.findById(postId);
+		if (post.isEmpty()) {
+			return ResponseEntity.badRequest()
+				.body(new Error(HttpStatus.BAD_REQUEST, ErrorMessage.NO_SUCH_POST));
+		}
+		String commentId;
+		if (commentRequestDto.getParentId() != null) {
+			Optional<Comment> parentComment =
+				commentService.findById(commentRequestDto.getParentId());
+			if (parentComment.isEmpty())
+				return ResponseEntity.badRequest()
+				.body(new Error(HttpStatus.BAD_REQUEST, ErrorMessage.NO_SUCH_PARENT_COMMENT));
+			commentId =
+				commentService.createChildComment(commentRequestDto,
+					user.get(), post.get(), parentComment.get());
+		} else
+			commentId = commentService.createComment(commentRequestDto, user.get(), post.get());
+		return ResponseEntity.created(
+			URI.create("/posts/" + postId + "/comments" + commentId)).build();
+	}
+}
