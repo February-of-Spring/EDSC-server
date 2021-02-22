49e469e16221240b1af1c71ee25339f0adb65969
hoit1302 <jueun000101@gmail.com>
2021-02-22 19:35:01 +0900

    feat(ErrorMessage): add error case

--- src/main/java/com/february/edsc/common/ErrorMessage.java
+++ src/main/java/com/february/edsc/common/ErrorMessage.java
@@ -9,4 +9,5 @@ public class ErrorMessage {
 	public static final String NO_SUCH_PARENT_CATEGORY = "카테고리의 상위 항목이 존재하지 않습니다.";
 	public static final String CATEGORY_HAS_POSTS = "게시물이 존재하는 카테고리는 삭제할 수 없습니다.";
 	public static final String CATEGORY_HAS_CHILD = "하위 카테고리가 존재하는 그룹은 삭제할 수 없습니다.";
+	public static final String NO_SUCH_PARENT_COMMENT = "상위 댓글이 존재하지 않습니다.";
 }
