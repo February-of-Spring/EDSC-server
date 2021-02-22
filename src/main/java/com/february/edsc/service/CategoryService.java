e6a89ffb8c39897f827fc6f54caa11d8e3caa0c3
hoit1302 <jueun000101@gmail.com>
2021-02-22 10:03:40 +0900

    feat(CategoryService): add get categories logic

--- src/main/java/com/february/edsc/service/CategoryService.java
+++ src/main/java/com/february/edsc/service/CategoryService.java
@@ -1,11 +1,15 @@
 package com.february.edsc.service;
 
+import com.february.edsc.domain.category.CategoryPackResponseDto;
 import com.february.edsc.domain.category.Category;
+import com.february.edsc.domain.category.CategoryResponseDto;
 import com.february.edsc.repository.CategoryRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -15,5 +19,29 @@ public class CategoryService {
 
 	public Optional<Category> findByName(String categoryName) {
 		return categoryRepository.findByName(categoryName);
+	}
+
+	@Transactional
+	public List<CategoryPackResponseDto> getCategories() {
+		List<CategoryPackResponseDto> categoryPackResponseDtoList = new ArrayList<>();
+		long id = 1;
+		for (Category parentCategory: categoryRepository.findAllByLevel(1)) {
+			List<CategoryResponseDto> child = getChild(parentCategory);
+			long parentNum = child.stream().mapToLong(CategoryResponseDto::getPostNum).sum();
+			categoryPackResponseDtoList.add(
+				CategoryPackResponseDto.builder()
+				.id(id++)
+				.parent(parentCategory.toCategoryParentResponseDto(parentNum))
+				.childNum((long) child.size())
+				.child(child)
+				.build());
+		}
+		return categoryPackResponseDtoList;
+	}
+
+	@Transactional
+	public List<CategoryResponseDto> getChild(Category category) {
+		return categoryRepository.findAllByParentIdAndLevel(category.getId(), 2)
+			.stream().map(Category::toCategoryResponseDto).collect(Collectors.toList());
 	}
 }
