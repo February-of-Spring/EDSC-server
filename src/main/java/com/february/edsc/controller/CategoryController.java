b4f8d437956a01dcb6dbdbdf139d3d0d8f949093
hoit1302 <jueun000101@gmail.com>
2021-02-22 12:06:31 +0900

    feat(CategoryController): GET /category/:level1/:level2

--- src/main/java/com/february/edsc/controller/CategoryController.java
+++ src/main/java/com/february/edsc/controller/CategoryController.java
@@ -1,10 +1,17 @@
 package com.february.edsc.controller;
 
+import com.february.edsc.common.Error;
+import com.february.edsc.common.ErrorMessage;
+import com.february.edsc.domain.category.Category;
 import com.february.edsc.service.CategoryService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.Optional;
 
 @Controller
 @AllArgsConstructor
@@ -12,8 +19,26 @@ public class CategoryController {
 
 	private final CategoryService categoryService;
 
-	@GetMapping("/categories")
+	@GetMapping("/category")
 	public ResponseEntity<Object> getCategories() {
 		return ResponseEntity.ok().body(categoryService.getCategories());
+	}
+
+	@GetMapping(value = {"/category/{level1}", "/category/{level1}/{level2}"})
+	public ResponseEntity<Object> getPostsByCategory(
+		@PathVariable("level1") String level1,
+		@PathVariable(name = "level2", required = false) Optional<String> level2) {
+		Optional<Category> category1 = categoryService.findByName(level1);
+		if (category1.isEmpty())
+			return ResponseEntity.badRequest()
+				.body(new Error(HttpStatus.BAD_REQUEST, ErrorMessage.NO_SUCH_CATEGORY));
+		if (level2.isEmpty()) {
+			return ResponseEntity.ok().body(categoryService.getPostsByParentCategory(category1.get()));
+		}
+		Optional<Category> category2 = categoryService.findByName(level2.get());
+		if (category2.isEmpty())
+			return ResponseEntity.badRequest()
+				.body(new Error(HttpStatus.BAD_REQUEST, ErrorMessage.NO_SUCH_CATEGORY));
+		return ResponseEntity.ok().body(categoryService.getPostsByChildCategory(category2.get()));
 	}
 }
