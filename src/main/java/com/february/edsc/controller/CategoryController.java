f62471e09fb360d212aef9fbf4ffc24071eb8d42
hoit1302 <jueun000101@gmail.com>
2021-02-22 14:34:30 +0900

    feat(CategoryController): add DELETE /category/:level1/:level2

--- src/main/java/com/february/edsc/controller/CategoryController.java
+++ src/main/java/com/february/edsc/controller/CategoryController.java
@@ -9,14 +9,12 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -76,5 +74,32 @@ public class CategoryController {
 		}
 		result = URLEncoder.encode("/category/" + result, StandardCharsets.UTF_8);
 		return ResponseEntity.created(URI.create(result)).build();
+	}
+
+	@DeleteMapping(value = {"/category/{level1}", "/category/{level1}/{level2}"})
+	public ResponseEntity<Object> deleteCategory(
+		@PathVariable("level1") String level1,
+		@PathVariable(name = "level2", required = false) Optional<String> level2) {
+		Optional<Category> category1 = categoryService.findByName(level1);
+		if (category1.isEmpty())
+			return ResponseEntity.badRequest()
+				.body(new Error(HttpStatus.BAD_REQUEST, ErrorMessage.NO_SUCH_CATEGORY));
+		if (level2.isEmpty()) {
+			List<Category> childCategories = categoryService.findAllByParentId(category1.get());
+			if (childCategories.size() != 1)
+				return ResponseEntity.badRequest()
+					.body(new Error(HttpStatus.BAD_REQUEST, ErrorMessage.CATEGORY_HAS_CHILD));
+			categoryService.deleteCategory(category1.get());
+		} else {
+			Optional<Category> category2 = categoryService.findByName(level2.get());
+			if (category2.isEmpty())
+				return ResponseEntity.badRequest()
+					.body(new Error(HttpStatus.BAD_REQUEST, ErrorMessage.NO_SUCH_CATEGORY));
+			if (category2.get().getPosts().size() != 0)
+				return ResponseEntity.badRequest()
+					.body(new Error(HttpStatus.BAD_REQUEST, ErrorMessage.CATEGORY_HAS_POSTS));
+			categoryService.deleteCategory(category2.get());
+		}
+		return ResponseEntity.noContent().build();
 	}
 }
