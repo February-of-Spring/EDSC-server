bcfcf75706e72a5112a67043796318cca780f370
hoit1302 <jueun000101@gmail.com>
2021-02-22 14:35:57 +0900

    feat(ErrorMessage): add error message

--- src/main/java/com/february/edsc/common/ErrorMessage.java
+++ src/main/java/com/february/edsc/common/ErrorMessage.java
@@ -7,4 +7,6 @@ public class ErrorMessage {
 	public static final String NO_SUCH_CATEGORY = "존재하지 않는 카테고리입니다.";
 	public static final String DUPLICATE_CATEGORY = "이미 존재하는 카테고리입니다.";
 	public static final String NO_SUCH_PARENT_CATEGORY = "카테고리의 상위 항목이 존재하지 않습니다.";
+	public static final String CATEGORY_HAS_POSTS = "게시물이 존재하는 카테고리는 삭제할 수 없습니다.";
+	public static final String CATEGORY_HAS_CHILD = "하위 카테고리가 존재하는 그룹은 삭제할 수 없습니다.";
 }
