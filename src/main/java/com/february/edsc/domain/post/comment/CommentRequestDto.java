cd5e138dc84bec11a99dd31b821baa267e1412c9
hoit1302 <jueun000101@gmail.com>
2021-02-22 19:33:56 +0900

    feat(CommentRequestDto): add dto

---
+++ src/main/java/com/february/edsc/domain/post/comment/CommentRequestDto.java
@@ -0,0 +1,24 @@
+package com.february.edsc.domain.post.comment;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class CommentRequestDto {
+	private final String email;
+	private final String content;
+	private final Boolean isPublic;
+	private final Long parentId;
+
+	public boolean isRequiredFieldNull() {
+		return (email == null) || (email.isEmpty()) ||
+		(content == null) || (content.isEmpty());
+	}
+
+	public boolean isUpdatingRequiredFieldNull() {
+		return (content == null) || (content.isEmpty());
+	}
+}
