c313fc9c90733b9779247811f76bd367063cf531
hoit1302 <jueun000101@gmail.com>
2021-02-24 01:22:58 +0900

    feat(UserController): add PATCH /users/:id/image

--- src/main/java/com/february/edsc/controller/UserController.java
+++ src/main/java/com/february/edsc/controller/UserController.java
@@ -4,22 +4,25 @@ import com.february.edsc.common.Error;
 import com.february.edsc.common.ErrorMessage;
 import com.february.edsc.domain.user.User;
 import com.february.edsc.domain.user.UserUpdateDto;
+import com.february.edsc.service.S3Service;
 import com.february.edsc.service.UserService;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
+import java.net.URI;
 import java.util.Optional;
 
 @Controller
 @AllArgsConstructor
 public class UserController {
 
+	private  final S3Service s3Service;
 	private final UserService userService;
 
 	@GetMapping("/users/{id}")
@@ -69,5 +72,31 @@ public class UserController {
 			return ResponseEntity.badRequest()
 				.body(new Error(HttpStatus.BAD_REQUEST, ErrorMessage.NO_SUCH_USER));
 		} return ResponseEntity.ok().body(userService.getUserLikes(id));
+	}
+
+	@PatchMapping("/users/{id}/image")
+	public ResponseEntity<Object> updateUserImage(
+		@RequestParam("image") MultipartFile multipartFile, @PathVariable Long id) throws IOException {
+		Optional<User> user = userService.findById(id);
+		if (user.isEmpty()) {
+			return ResponseEntity.badRequest().body(
+				new Error(HttpStatus.BAD_REQUEST, ErrorMessage.NO_SUCH_USER));
+		}
+		if (multipartFile.isEmpty()) {
+			return ResponseEntity.badRequest().body(
+				new Error(HttpStatus.BAD_REQUEST, ErrorMessage.REQUIRED_FIELD_NULL));
+		}
+		Optional<File> convertedFile = s3Service.convert(multipartFile);
+		if (convertedFile.isEmpty()) {
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+				.body(new Error(HttpStatus.INTERNAL_SERVER_ERROR,
+					ErrorMessage.FAIL_FILE_CONVERT));
+		}
+		if (!s3Service.isValidExtension(convertedFile.get())) {
+			return ResponseEntity.badRequest().body(
+				new Error(HttpStatus.BAD_REQUEST, ErrorMessage.INVALID_FILE_TYPE));
+		}
+		String result = userService.updateUserImage(convertedFile.get(), user.get());
+		return ResponseEntity.created(URI.create(result)).build();
 	}
 }
