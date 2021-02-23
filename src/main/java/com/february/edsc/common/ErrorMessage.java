b3d704dfb4cdf29d4dfe3378db66cf34715bf65a
hoit1302 <jueun000101@gmail.com>
2021-02-23 14:09:30 +0900

    refactor(ErrorMessage): change error description

--- src/main/java/com/february/edsc/common/ErrorMessage.java
+++ src/main/java/com/february/edsc/common/ErrorMessage.java
@@ -11,7 +11,7 @@ public class ErrorMessage {
 	public static final String CATEGORY_HAS_CHILD = "하위 카테고리가 존재하는 그룹은 삭제할 수 없습니다.";
 	public static final String NO_SUCH_COMMENT = "존재하지 않는 댓글입니다.";
 	public static final String NO_SUCH_PARENT_COMMENT = "상위 댓글이 존재하지 않습니다.";
-	public static final String BAD_REQUEST = "잘못된 경로입니다.";
+	public static final String BAD_REQUEST = "잘못된 경로로 접근하셨습니다.";
 	public static final String UNAUTHORIZED_TO_UPDATE = "수정할 수 있는 권한이 없습니다.";
 	public static final String UNAUTHORIZED_TO_DELETE = "삭제할 수 있는 권한이 없습니다.";
 }
