994cc83fa845bdced9507b60b45730705c988b36
hoit1302 <jueun000101@gmail.com>
2021-02-24 12:52:22 +0900

    feat(PostRepository): add query method to get main posts

--- src/main/java/com/february/edsc/repository/PostRepository.java
+++ src/main/java/com/february/edsc/repository/PostRepository.java
@@ -4,7 +4,9 @@ import com.february.edsc.domain.post.Post;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface PostRepository extends CrudRepository<Post, Long> {
 	List<Post> findAllByUserId(Long userId);
+	Optional<Post> findTopByCategoryIdOrderByCreatedAtDesc(long id);
 }
