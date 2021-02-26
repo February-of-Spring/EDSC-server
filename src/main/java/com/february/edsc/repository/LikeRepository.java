7bd8f1fcfcf4324da9d609496994258e76d08e89
hoit1302 <jueun000101@gmail.com>
2021-02-26 18:01:48 +0900

    feat(LikeRepository, PostRepository): add pageable to sort and pagination

--- src/main/java/com/february/edsc/repository/LikeRepository.java
+++ src/main/java/com/february/edsc/repository/LikeRepository.java
@@ -1,13 +1,13 @@
 package com.february.edsc.repository;
 
 import com.february.edsc.domain.user.like.Like;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.Collection;
-import java.util.List;
 import java.util.Optional;
 
 public interface LikeRepository extends CrudRepository<Like, Long> {
-	List<Like> findAllByUserId(Long userId);
+	Page<Like> findAllByUserId(Long userId, Pageable pageable);
 	Optional<Like> findByPostIdAndUserId(Long postId, Long UserId);
 }
--- src/main/java/com/february/edsc/repository/PostRepository.java
+++ src/main/java/com/february/edsc/repository/PostRepository.java
@@ -1,12 +1,13 @@
 package com.february.edsc.repository;
 
 import com.february.edsc.domain.post.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface PostRepository extends CrudRepository<Post, Long> {
-	List<Post> findAllByUserId(Long userId);
+	Page<Post> findAllByUserId(Long userId, Pageable pageable);
 	Optional<Post> findTopByCategoryIdOrderByCreatedAtDesc(long id);
 }
