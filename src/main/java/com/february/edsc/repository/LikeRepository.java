fd56801846f73211d04f3e4b65a2e0f00548b41c
hoit1302 <jueun000101@gmail.com>
2021-02-27 21:37:25 +0900

    fix(LikeRepository, PostRepository): extend JpaRepository to pagination and sort

--- src/main/java/com/february/edsc/repository/LikeRepository.java
+++ src/main/java/com/february/edsc/repository/LikeRepository.java
@@ -3,11 +3,11 @@ package com.february.edsc.repository;
 import com.february.edsc.domain.user.like.Like;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface LikeRepository extends CrudRepository<Like, Long> {
+public interface LikeRepository extends JpaRepository<Like, Long> {
 	Page<Like> findAllByUserId(Long userId, Pageable pageable);
 	Optional<Like> findByPostIdAndUserId(Long postId, Long UserId);
 }
--- src/main/java/com/february/edsc/repository/PostRepository.java
+++ src/main/java/com/february/edsc/repository/PostRepository.java
@@ -3,11 +3,11 @@ package com.february.edsc.repository;
 import com.february.edsc.domain.post.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface PostRepository extends CrudRepository<Post, Long> {
+public interface PostRepository extends JpaRepository<Post, Long> {
 	Page<Post> findAllByUserId(Long userId, Pageable pageable);
 	Optional<Post> findTopByCategoryIdOrderByCreatedAtDesc(long id);
 }
